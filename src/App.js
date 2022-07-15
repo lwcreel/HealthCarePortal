9e4836fb98914715de943c20a372b1383d8b11b0
Creel, Landon <Landon_Creel@Dell.com>
2022-07-15 12:21:44 -0500

    part 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import logo from './App-Logo.png';
+import logo from './resources/App-Logo.png';
+import userLogo from './resources/User-Logo.jpg';
+import adminLogo from './resources/Admin-Logo.jpg';
+import { Container, Row, Col, Image } from 'react-bootstrap'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HealthCare Portal
-        </p>
-        
-      </header>
-    </div>
+    <body className="App">
+      <Container fluid>
+        <Row>
+          <Col style={{ padding: 10 }}>
+            <img src={logo}
+              className="App-logo"
+              alt="logo" />
+            <p> HealthCare Portal </p>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={6}>
+            <img className="Link-logo" src={userLogo} alt="User Portal Icon" />
+            <h4>User Login</h4>
+          </Col>
+          <Col md={6}>
+            <img className="Link-logo" src={adminLogo} alt="Admin Portal Icon" />
+            <h4>Admin Login</h4>
+          </Col>
+        </Row>
+      </Container>
+    </body>
   );
 }
 
