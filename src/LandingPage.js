6e2e56c83509870ec42c454758dfd8a96e760711
Creel, Landon <Landon_Creel@Dell.com>
2022-07-18 12:36:01 -0500

    Added routing to images

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -2,6 +2,7 @@ import userLogo from './resources/User-Logo.jpg';
 import adminLogo from './resources/Admin-Logo.jpg';
 import logo from './resources/App-Logo.png';
 import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function LandingPage() {
 
@@ -18,11 +19,15 @@ function LandingPage() {
 
             <Row>
                 <Col md={6}>
-                    <img className="Link-logo" src={userLogo} alt="User Portal Icon" />
+                    <Link to="/login">
+                        <img className="Link-logo" src={userLogo} alt="User Portal Icon" />
+                    </Link>
                     <h4>User Login</h4>
                 </Col>
                 <Col md={6}>
-                    <img className="Link-logo" src={adminLogo} alt="Admin Portal Icon" />
+                    <Link to="/login">
+                        <img className="Link-logo" src={adminLogo} alt="Admin Portal Icon" />
+                    </Link>
                     <h4>Admin Login</h4>
                 </Col>
             </Row>
