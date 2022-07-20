c64ac01ddf22c19b0757d8a11c452877d804756e
Creel, Landon <Landon_Creel@Dell.com>
2022-07-20 13:49:53 -0500

    Aligned user dashboard

--- src/components/User/Dashboard.js
+++ src/components/User/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Navbar } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import medLogo from './../../resources/med-bottle-logo.jpg'
 import reportLogo from './../../resources/report-logo.png'
@@ -7,19 +7,16 @@ import NavBar from './UserNavbar';
 
 export default function Dashboard() {
   return (
-    
-    <div>
-    <NavBar />
-
-    <Container fluid >
-      <Row>
-        <h2>User Dashboard</h2>
-      </Row>
-      <Row>
-        <Col md={6}><Link to="/user/medicine"><img className="Link-logo" src={medLogo} alt="Search for Meds" /></Link></Col>
-        <Col md={6}><Link to="/user/status"><img className="Link-logo" src={reportLogo} alt="Order Status" /></Link></Col>
-      </Row>
-    </Container>
+    <div className="dash-wrapper">
+      <Container fluid>
+        <Row><NavBar /></Row>
+        <Row><h2>User Dashboard</h2></Row>
+        <Row>
+          <Col md={6}><Link to="/user/medicine"><img className="Dash-logos" src={medLogo} alt="Search for Meds" /></Link></Col>
+          <Col md={6}><Link to="/user/status"><img className="Dash-logos" src={reportLogo} alt="Order Status" /></Link></Col>
+        </Row>
+      </Container>
+      <h5 align="right">Funds: $1000</h5>
     </div>
-    );
+  );
 }
--- src/components/User/UserNavbar.js
+++ src/components/User/UserNavbar.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import medLogo from './../../resources/med-bottle-logo.jpg'
-import reportLogo from './../../resources/report-logo.png'
+import { Nav } from 'react-bootstrap';
 
 class NavBar extends Component {
 
     render() {
         return (
-            <Navbar bg="light" expang="lg">
-                <Container>
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#cart">Cart</Nav.Link>
-                    <Nav.Link href="#status">Order Status</Nav.Link>
-                    <Nav.Link href="#funds">Funds</Nav.Link>
-                </Container>
-            </Navbar>
+            <Nav>
+                <Nav.Link> Home </Nav.Link>
+                <Nav.Link> Cart </Nav.Link>
+                <Nav.Link> Status </Nav.Link>
+                <Nav.Link> Funds </Nav.Link>
+            </Nav>
         );
     }
 }
