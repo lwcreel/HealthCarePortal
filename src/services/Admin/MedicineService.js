4736b352acb7840110939ae5da2f4096259befd7
Creel, Landon <Landon_Creel@Dell.com>
2022-09-26 14:03:06 -0500

    added backend connection for addMed

--- src/services/Admin/MedicineService.js
+++ src/services/Admin/MedicineService.js
@@ -1,7 +1,23 @@
+import axios from "axios";
+
 export function addMedicine(data) {
 
     console.log(data);
-    return data;
+    let newMedicine = {
+        "quantity": data.quantity,
+        "medName": data.medName,
+        "companyName": data.companyName,
+        "uses": data.uses,
+        "expDate": data.expDate,
+        "costFactor": data.price,
+        "userList": []
+    };
+
+    axios({
+        url: "http://localhost:8080/medicines",
+        method: "POST",
+        data: newMedicine
+    })
 }
 
 export function updateMedicine(data) {
