945c0fd80413709b7b6b84ac7bf447f9cbd8cfe7
Creel, Landon <Landon_Creel@Dell.com>
2022-09-26 13:48:55 -0500

    fixed report form

--- src/components/Admin/Forms/ReportForm.js
+++ src/components/Admin/Forms/ReportForm.js
@@ -4,16 +4,16 @@ export default function ReportForm() {
     return (
         <Container fluid>
             <div>
-                <form >
+                <form>
                     <Row>
                         <label>
                             <p>Select a report to generate:</p>
-                            <select id="allowedReports">
-                                <option value="Stock Report" />
-                                <option value="Sales Report" />
-                                <option value="Medicine Report" />
-                                <option value="Order Report" />
-                                <option value="Data Report" />
+                            <select name="reports" id="allowedReports">
+                                <option value="Stock Report">Stock Report</option>
+                                <option value="Sales Report">Sales Report</option>
+                                <option value="Medicine Report">Medicinte Report</option>
+                                <option value="Order Report">Order Report</option>
+                                <option value="Data Report">Data Report</option>
                             </select>
                         </label>
                     </Row>
